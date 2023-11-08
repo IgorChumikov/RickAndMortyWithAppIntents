68f37babbe3317ef0a4590054a806add602f89b6
Igor <chumikov.igor@mail.ru>
2023-11-08 18:20:56 +0700

    add

---
+++ RickAndMortyWithAppIntents/RickAndMortyWithAppIntents/RickAndMortyWithAppIntentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyWithAppIntentsApp.swift
+//  RickAndMortyWithAppIntents
+//
+//  Created by Игорь Чумиков on 08.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyWithAppIntentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
